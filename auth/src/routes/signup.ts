49baaed50a13e939f722855105b3fe5600b5e773
SorenErvik <ervik.soren@gmail.com>
2024-03-12 10:57:46 -0500

    Finalized password validation in auth

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -6,14 +6,14 @@ const signupRouter = express.Router();
 
 signupRouter.post('/api/auth/signup', [
     body('email').isEmail().withMessage('Email must be in a valid format'), 
-body('password').isLength({ min: 8, max: 32 }).withMessage('Password must be between 8 adn 32 characters in length'),
+body('password').trim().isLength({ min: 8, max: 32 }).withMessage('Password must be between 8 adn 32 characters in length'),
 body('password').matches(/^(.*[a-z].*)$/).withMessage('Password must contain at least one lowercase letter'),
 body('password').matches(/^(.*[A-Z].*)$/).withMessage('Password must contain at least one uppercase letter'),
 body('password').matches(/^(.*[0-9].*)$/).withMessage('Password must contain at least one number')
 ], (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        res.status(422).send({});
+        res.status(422).send({errors: errors.array()});
     }
     res.send({});
 })
